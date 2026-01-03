51c666b543ce4c5ddf1cb9048fc5c83cf2e3b4fa
SirCotare <sircotare@3025-game.de>
2026-01-03 13:26:43 +0100

    add env variable to toggle steam validation

--- files/start.sh
+++ files/start.sh
@@ -24,7 +24,12 @@ echo " "
 echo "Installing/Updating Aska Dedicated Server files..."
 echo " "
 
-$steamcmd +@sSteamCmdForcePlatformType windows +force_install_dir "$server_files" +login anonymous +app_update 3246670 validate +quit
+validate_flag="validate"
+if [ "$NO_VALIDATE" = "true" ]; then
+  validate_flag=""
+fi
+
+$steamcmd +@sSteamCmdForcePlatformType windows +force_install_dir "$server_files" +login anonymous +app_update 3246670 $validate_flag +quit
 exit_code=$?
 
 if [ $exit_code -ne 0 ]; then
